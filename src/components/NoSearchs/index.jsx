d2fb5193237e258f11ba744dcf94be681174ca88
Jesús Molano <jessumolano@gmail.com>
2022-06-28 14:31:53 +0100

    Added styles and builded the project

--- src/components/NoSearchs/index.jsx
+++ src/components/NoSearchs/index.jsx
@@ -2,9 +2,9 @@ import './no-search.css'
 const NoSearch = () => {
   return (
     <div className='no-search'>
-      <img src="/src/assets/arrow-left.png" alt="arrow" />
+      <img src="src/assets/arrow-left.png" alt="arrow" />
       <span>Do a search in the bar above</span>
-      <img src="/src/assets/arrow-left.png" alt="arrow" />
+      <img src="src/assets/arrow-left.png" alt="arrow" />
     </div>
   )
 }
